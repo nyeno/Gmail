964770df847544b6e33d4e89b1f0a0816760a64c
Eyimofe Blessing-Kayode <mofekayode@Eyimofes-MacBook-Pro-2.local>
2022-08-15 14:19:58 -0700

    things are done

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,20 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import SideNav from '../components/SideNav'
-
-import Navbar from '../Shared/NavItem'
-import Search from '../Shared/Search'
-import Settings from '../Shared/Settings'
-
-import styles from '../styles/Home.module.css'
-
+import { useTheme } from 'next-themes'
 export default function Home() {
+  const {theme, setTheme} = useTheme('light')
+  console.log(theme)
   return (
-    <div className={styles.container}>
+   
+    <div>
       <Head>
         <title>Gmail</title>
         <meta name="description" content="Gmail Clone" />
         <link rel="icon" href="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico" />
       </Head>
+      <div className='h-screen dark:bg-black bg-gray-100'>
 
-      <main className={styles.main}>
-        <Navbar/>
-        <Search/>
-        <Settings/>
-        <SideNav />
-      </main>
+      </div>
 
-      <footer className={styles.footer}>
-        
-      </footer>
     </div>
   )
 }
