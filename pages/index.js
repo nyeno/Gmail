7e2ae6ad13222f19076bcb873aaf785951d94c7f
Laura Eno <enonyenooke@gmail.com>
2022-08-18 20:29:54 +0100

    Mofe deleted stuff

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Head from 'next/head'
 import { useTheme } from 'next-themes'
+import Image from 'next/image'
+
+import Checkbox from '@mui/material/Checkbox';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import TuneIcon from '@mui/icons-material/Tune';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AppsIcon from '@mui/icons-material/Apps';
+
 export default function Home() {
   const {theme, setTheme} = useTheme('light')
   console.log(theme)
@@ -12,6 +22,21 @@ export default function Home() {
         <link rel="icon" href="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico" />
       </Head>
       <div className='h-screen dark:bg-black bg-gray-100'>
+        <nav className='flex items-center'>
+          <MenuIcon />
+          <Image src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r2.png" alt='Gmail Logo' width={90} height={30}/>
+          <form>
+            <SearchIcon />
+            <input type="text" />
+            <TuneIcon />
+          </form> 
+          <HelpOutlineIcon />
+          <SettingsIcon />
+          <AppsIcon />
+        </nav>
+        <nav>
+          
+        </nav>
       </div>
 
     </div>
