c67b93c10f2af13f6b3a75a03c84509687e2da36
Laura Eno <enonyenooke@gmail.com>
2022-08-20 20:13:36 +0100

    Added icons for nav bar

---
+++ Shared/NavItem.js
@@ -0,0 +1,10 @@
+import { Checkbox } from "@mui/material"
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
+
+const NavItem = (icon) => {
+    let IconName = this.props.icon;
+    
+}
+
+export default NavItem
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,22 @@ import Head from 'next/head'
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
-import Checkbox from '@mui/material/Checkbox';
+import { Checkbox } from '@mui/material';
+import InboxIcon from '@mui/icons-material/Inbox';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import TuneIcon from '@mui/icons-material/Tune';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AppsIcon from '@mui/icons-material/Apps';
+import NavItem from '../Shared/NavItem';
+import Star from '@mui/icons-material/Star';
+import StarBorder from '@mui/icons-material/StarBorder';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
+import ScheduleSendOutlinedIcon from '@mui/icons-material/ScheduleSendOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 export default function Home() {
   const {theme, setTheme} = useTheme('light')
@@ -35,8 +44,38 @@ export default function Home() {
           <AppsIcon />
         </nav>
         <nav>
-          
+          <div className="flex">
+            <InboxIcon/>
+            <p>Inbox</p>
+          </div>
+          <div className="flex">
+            <StarBorder/>
+            <p>Starred</p>
+          </div>
+          <div className="flex">
+            <SendOutlinedIcon/>
+            <p>Sent</p>
+          </div>
+          <div className="flex">
+            <InsertDriveFileOutlinedIcon/>
+            <p>Drafts</p>
+          </div>
+          <div className="flex">
+            <ScheduleSendOutlinedIcon/>
+            <p>Scheduled</p>
+          </div>
+          <div className="flex">
+            <EmailOutlinedIcon/>
+            <p>All Mail</p>
+          </div>
+          <div className="flex">
+            <DeleteOutlinedIcon />
+            <p>Trash</p>
+          </div>
         </nav>
+        <main>
+
+        </main>
       </div>
 
     </div>
