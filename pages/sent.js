53a8b92b041b95ffdd7f56e866e0265844d33231
Laura Eno <enonyenooke@gmail.com>
2022-09-13 12:01:14 +0100

    Worked on existing sign up  page

--- pages/sent.js
+++ pages/sent.js
@@ -17,14 +17,14 @@ export default function Home() {
       <div className="h-screen dark:bg-black bg-gray-100">
       <main className="align-middle p-8 border border-[#DADCE0] rounded h-4/6 w-2/6 m-auto">
           <section>
-              <Image src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png" width={75} height={28} className=""/>
-              <p className="text-2xl">Sign in
+              <div className="flex justify-center"><Image src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png" width={75} height={28} /></div>
+              <p className="text-2xl text-center">Sign in
                 <span className="block text-sm my-4">to continue to Gmail</span>
               </p>
               <form className="my-6">
-                  <input type="text" className="border border-[#DADCE0] rounded px-2 py-2 text-sm " placeholder="Email or phone"/>
-                  <p className="text-blue-600 text-sm font-medium">Forgot email?</p>
-                  <p className="text-sm">Not your computer? Use Guest mode to sign in privately
+                  <input type="text" className="border border-[#DADCE0] rounded px-2 py-2 w-full" placeholder="Email or phone"/>
+                  <p className="text-blue-600 text-sm font-medium mt-2">Forgot email?</p>
+                  <p className="text-sm my-8">Not your computer? Use Guest mode to sign in privately
                     <span className="text-blue-600 text-sm font-medium block">Learn more</span>
                   </p>
                   <div className=" col-span-2 flex justify-between items-center">
